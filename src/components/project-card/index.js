176ec77575321470d4231c79e4b4ab4a6c290297
Oxana Theis <otheis@protonmail.com>
2024-02-19 14:06:17 +0000

    add projects

--- src/components/project-card/index.js
+++ src/components/project-card/index.js
@@ -2,8 +2,12 @@ import React, { useEffect, useState, useRef } from "react";
 
 import "./style.css";
 import { cn as bem } from "@bem-react/classname";
-import network_img from "../../assets/network.jpg"
-import dashboard_v1 from "../../assets/dashboard_v1.jpg"
+// import network_img from "../../assets/network.jpg"
+// import dashboard_v1 from "../../assets/dashboard_v1.jpg"
+
+import network_img from "../../assets/test_1.png";
+import dashboard_v1 from "../../assets/test_2.png";
+
 // import network_img from "../../assets/network_img_tel.jpg"
 // import network_img from "../../assets/network_img_v3.jpg"
 // import network_img from "../../assets/network_img_v4.jpg"
@@ -65,13 +69,77 @@ import { Controller, Scene } from "react-scrollmagic";
 // export default ProjectCard;
 
 const ProjectCard = () => {
+  let networkTitle = ["I", "N", "T", "R", "A", "L", "I", "N", "K"];
+  let sportTitle = ["S", "P", "O", "R", "T", "S", "E", "E"];
+  // useRef for networkTitle letters
+  const networkTitleRef = useRef([]);
+  // useRef for sportTitle letters
+  const sportTitleRef = useRef([]);
+  const [networkUnderlineWidth, setNetworkUnderlineWidth] = useState(0);
+  const [sportUnderlineWidth, setSportUnderlineWidth] = useState(0);
+  const projectTitle = "A secure and streamlined platform designed to enhance corporate communications";
+  const project_Title = "An ultimate hub for fitness activities monitoring ";
+  // State to handle the hover effect
+  const [isNetworkTitleHovered, setNetworkTitleIsHovered] = useState(false);
+  const [isSportTitleHovered, setSportTitleIsHovered] = useState(false);
+  // Event handlers
+  const handleMouseEnterNetwork = () => setNetworkTitleIsHovered(true);
+  const handleMouseLeaveNetwork = () =>setNetworkTitleIsHovered(false);
+
+
+
+  const handleMouseEnterSport = () => setSportTitleIsHovered(true);
+  const handleMouseLeaveSport = () =>setSportTitleIsHovered(false);
+
+  useEffect(() => {
+    let networkTotalWidth = 0;
+    networkTitleRef.current.forEach((element) => {
+      if (element) {
+        networkTotalWidth += element.offsetWidth;
+      }
+    });
+    setNetworkUnderlineWidth(networkTotalWidth);
+
+    let sportTotalWidth = 0;
+    sportTitleRef.current.forEach((element) => {
+      if (element) {
+        sportTotalWidth += element.offsetWidth;
+      }
+    });
+    setSportUnderlineWidth(sportTotalWidth);
+  }, []);
+
   return (
     <div className="parallax-container">
       <Controller>
         <Scene duration={"100%"} triggerHook={0.2} pin triggerElement={".test"}>
           <div className="test" style={{ display: "flex", width: "50%", flexDirection: "column", height: "100vh", position: "relative 0!important" }}>
-            <div className="fixed-content">Fixed Content1</div>
-            <div className="fixed-contentt">Fixed Content2</div>
+            <span className="fixed-content" onMouseEnter={handleMouseEnterNetwork} onMouseLeave={handleMouseLeaveNetwork}>
+              {networkTitle.map((letter, index) => {
+                return (
+                  <span key={index} className="fixed-content-letter" ref={(el) => (networkTitleRef.current[index] = el)}>
+                    {letter}
+                  </span>
+                );
+              })}
+              <div className="underline" style={{ width: networkUnderlineWidth }} />
+              <div className="project-card-title">
+                <p> {projectTitle.toLocaleUpperCase()}</p>
+              </div>
+            </span>
+            <span className="fixed-contentt" onMouseEnter={ handleMouseEnterSport } onMouseLeave={handleMouseLeaveSport}>
+              {sportTitle.map((letter, index) => {
+                return (
+                  <span key={index} className="fixed-content-letter" ref={(el) => (sportTitleRef.current[index] = el)}>
+                    {letter}
+                  </span>
+                );
+              })}
+              <div className="underline" style={{ width: sportUnderlineWidth }} />
+              <div className="project-card-title">
+                <p> {project_Title.toLocaleUpperCase()}</p>
+              </div>
+            </span>
           </div>
         </Scene>
       </Controller>
@@ -79,11 +147,28 @@ const ProjectCard = () => {
       {/* Scrollable content */}
       <div className="scroll-content">
         <div className="top-right">
-          <img src={network_img} alt=""></img>
+          <img
+            src={network_img}
+            alt=""
+            style={{
+              transform: isNetworkTitleHovered ? "scale(1.1)" : "scale(1)",
+              transition: "transform 1.5s cubic-bezier(0.62, 0.05, 0.01, 0.99)",
+            }}
+          ></img>
         </div>
-        {/* The bottom-left was commented out before so left commented */}
-        <div className="bottom-right"> <img src={dashboard_v1} alt=""></img></div>
-        {/* <div className="bottom-left">Bottom Left Content</div> */}
+ 
+        <div className="bottom-right">
+          {" "}
+          <img
+            src={dashboard_v1}
+            alt=""
+            style={{
+              transform: isSportTitleHovered ? "scale(1.2)" : "scale(1)",
+              transition:  "transform 1.5s cubic-bezier(0.62, 0.05, 0.01, 0.99)",
+            }}
+          ></img>
+        </div>
+        
       </div>
     </div>
   );
