67649204673b7b037cd48b3f9aa6de4d6082bfb7
Oxana Theis <otheis@protonmail.com>
2024-03-01 15:53:05 +0000

    add title contact animation

--- src/components/contact-footer/index.js
+++ src/components/contact-footer/index.js
@@ -17,9 +17,10 @@ const ContactFooter = () => {
   const networkTitleRef = useRef([]);
   const { hasNavClicked, setHasNavClicked } = useNavClick();
   const { hasHomepageClicked } = useHomeClick();
-//handle the state of elements (titles etc)
-const [isVisible, setIsVisible] = useState(false);
-const [networkUnderlineWidth, setNetworkUnderlineWidth] = useState(0);
+  //handle the state of elements (titles etc)
+  const [isVisible, setIsVisible] = useState(false);
+  const [networkUnderlineWidth, setNetworkUnderlineWidth] = useState(0);
+  const [isEmailVisible, setEmailIsVisible] = useState(false);
   /*Displays the navBar on the contact 
   section only when we are in this section.
   Delays the display after click from main page on 500ms;
@@ -29,7 +30,7 @@ const [networkUnderlineWidth, setNetworkUnderlineWidth] = useState(0);
 
   useEffect(() => {
     const navBar = document.querySelector(".Nav-bar-wrapper");
-
+    let timer1, timer2;
     let timer;
 
     if (location.pathname === "/contact") {
@@ -38,50 +39,58 @@ const [networkUnderlineWidth, setNetworkUnderlineWidth] = useState(0);
           navBar.classList.add("visible");
           //save to storage ,otherwise the navBar is not visible on page reload
           sessionStorage.setItem("navBarVisible", "true");
+
           // setIsVisible(true);
         }
       }, 500);
-    } 
+      timer1 = setTimeout(() => {
+        setIsVisible(true);
+      }, 1800); // Adjust the timeout as needed
+      timer2 = setTimeout(() => {
+        setEmailIsVisible(true);
+      }, 4500);
+
+    
+    }
     // else {
     //   sessionStorage.clear();
     // }
 
-    return () => clearTimeout(timer);
+    return () => {
+      clearTimeout(timer1);
+      clearTimeout(timer2);
+      clearTimeout(timer);
+    };
   }, [location.pathname]);
 
   // Check session storage for NavBar visibility state
   const isNavBarVisible = sessionStorage.getItem("navBarVisible") === "true";
 
-
-
-
-
   /*need to handle the scrollY to display 
   services elements in the main page by adding visible class in jsx*/
-  const path = window.location.pathname;
   const handleScroll = () => {
-  
+    const path = window.location.pathname;
+
     // Conditions for Home ("/") Page
     if (path === "/") {
       if (window.scrollY >= 3700) {
         setIsVisible(true);
       }
-    
-      // if (window.scrollY >= 3300) {
-      //   // setSecondTitleIsVisible(true);
-      // }
+      if (window.scrollY >= 4100) {
+        console.log('3900')
+        setEmailIsVisible(true);
+      }
+   
     }
     // Condition for Non-Home (other) Pages
     else {
-      // if (window.scrollY >= 200) {
-        setIsVisible(true);
-      // }
-      // if (window.scrollY >= 1200) {
-      //   // setSecondTitleIsVisible(true);
-      // }
+   
+      setIsVisible(true);
+      setEmailIsVisible(true);
+  
     }
   };
-  
+
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
 
@@ -91,6 +100,26 @@ const [networkUnderlineWidth, setNetworkUnderlineWidth] = useState(0);
   }, []);
 
 
+
+  // useEffect(() => {
+  //   const handleInitialVisibility = () => {
+  //     const path = window.location.pathname;
+  //     if (path !== "/") {
+  //       setIsVisible(true);
+  //       setEmailIsVisible(true);
+  //     }
+  //   };
+  
+  //   // Set initial visibility based on the current path
+  //   handleInitialVisibility();
+  
+  //   // Setup the event listener for scrolling
+  //   window.addEventListener("scroll", handleScroll);
+  
+  //   return () => {
+  //     window.removeEventListener("scroll", handleScroll);
+  //   };
+  // }, []);
 
 
 
@@ -108,9 +137,8 @@ const [networkUnderlineWidth, setNetworkUnderlineWidth] = useState(0);
 
   const wrappedText = wrapTextIntoLines(text);
 
-  let email= ["C","O","N","T","A","C","T","&","F", "R", "O", "M", "S", "C", "R", "A","T","C","H",".","C","O","M"];
-
-
+  let email = ["C", "O", "N", "T", "A", "C", "T", "@", "F", "R", "O", "M", "S", "C", "R", "A", "T", "C", "H", ".", "C", "O", "M"];
+  // let email = ["C", "O", "N", "T", "A", "C", "T", "@"];
   useEffect(() => {
     let networkTotalWidth = 0;
     networkTitleRef.current.forEach((element) => {
@@ -119,58 +147,50 @@ const [networkUnderlineWidth, setNetworkUnderlineWidth] = useState(0);
       }
     });
     setNetworkUnderlineWidth(networkTotalWidth);
-
-    // let sportTotalWidth = 0;
-    // sportTitleRef.current.forEach((element) => {
-    //   if (element) {
-    //     sportTotalWidth += element.offsetWidth;
-    //   }
-    // });
-    // setSportUnderlineWidth(sportTotalWidth);
   }, []);
 
-
-  const underlineDelay = email.length * 0.07 + 0.14;
+  const underlineDelay = 1.30;
 
   return (
     <div className="section">
-
- 
       {hasNavClicked || hasHomepageClicked || isNavBarVisible ? <NavBar /> : ""}
-    <section className="contact-section">
-
-      <div className="contact-wrapper" >
-        <div className="contact-heading">
-          <h2 className={`contact-heading-number ${isVisible ? "visible" : ""}`} style={path === "/" ? { transitionDelay: '1s' } : {}}>04/</h2>
-          <p className={`contact-heading-title ${isVisible ? "visible" : ""}`} style={path === "/" ? { transitionDelay: '1s' } : {}}>{"GOT A PROJECT IN MIND?"}</p>
+      <section className="contact-section">
+        <div className="contact-wrapper">
+          <div className="contact-heading">
+            <h2 className={`contact-heading-number ${isVisible ? "visible" : ""}`} style={location.pathname === "/" ? { transitionDelay: "1s" } : {}}>
+              04/
+            </h2>
+            <p className={`contact-heading-title ${isVisible ? "visible" : ""}`} style={location.pathname === "/" ? { transitionDelay: "1s" } : {}}>
+              {"GOT A PROJECT IN MIND?"}
+            </p>
+          </div>
+          <p className={`contact-heading-message ${isVisible ? "visible" : ""}`} style={location.pathname === "/" ? { transitionDelay: "1s" } : {}}>
+            {"SEND A MESSAGE"}
+          </p>
         </div>
-        <p className={`contact-heading-message ${isVisible ? "visible" : ""}`} style={path === "/" ? { transitionDelay: '1s' } : {}} >{"SEND A MESSAGE"}</p>
-      </div>
-      <div className="contact-about">
-        {/* <h3>ABOUT</h3> */}
-        <p className={`contact-about-content ${isVisible ? "visible" : ""}`} > {wrappedText} </p>
-      </div>
+        <div className="contact-about">
+          {/* <h3>ABOUT</h3> */}
+          <p className={`contact-about-content ${isVisible ? "visible" : ""}`}> {wrappedText} </p>
+        </div>
 
-      <div className={cn("footer")}>
- 
-        {/* <div> */}
+        <div className="Contact-footer">
+          {/* <div> */}
 
-        <span className="footer-email">
-              {email.map((letter, index) => {
-                const style = { transitionDelay: `${index * 0.07}s` };
-                return (
-                  <span key={index} className={`"footer-email-letter" ${isVisible ? "visible" : ""}`} ref={(el) => (networkTitleRef.current[index] = el)} style={style}>
-                    {letter}
-                  </span>
-                );
-              })}
-              <div className={`underline ${isVisible ? "visible" : ""}`} style={{ width: networkUnderlineWidth, transitionDelay: `${underlineDelay}s` }} />
-             
-            </span>
-   
-      </div>
-      
-    </section>
+          <span className="footer-email">
+            {email.map((letter, index) => {
+              const baseDelay = 0; // Starting delay for the first span
+              const incrementalDelay = 0.07; // Delay to add for each subsequent span
+              const style = { transitionDelay: `${baseDelay + index * incrementalDelay}s` };
+              return (
+                <span key={index} className={`footer-email-letter ${isEmailVisible ? "visible" : ""}`} ref={(el) => (networkTitleRef.current[index] = el)} style={style}>
+                  {letter}
+                </span>
+              );
+            })}
+            <div className={`underline ${isEmailVisible ? "visible" : ""}`} style={{ width: networkUnderlineWidth, transitionDelay: `${underlineDelay}s` }} />
+          </span>
+        </div>
+      </section>
     </div>
   );
 };
